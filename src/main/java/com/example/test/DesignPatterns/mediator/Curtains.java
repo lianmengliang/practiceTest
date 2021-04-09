533578d5377e80307a8554820c5dc86774128a65
lianmengliang <18238691710@163.com>
2021-04-09 18:41:39 +0800

    中介者模式

---
+++ src/main/java/com/example/test/DesignPatterns/mediator/Curtains.java
@@ -0,0 +1,22 @@
+package com.example.test.DesignPatterns.mediator;
+
+/**
+ * @Author ： Leo
+ * @Date : 2021/4/9 15:01
+ * @Desc:
+ */
+public class Curtains extends Colleague {
+    public Curtains(Mediator mediator, String name) {
+        super(mediator, name);
+        mediator.Register(name,this);
+    }
+
+    @Override
+    void sendMessage(int stateChange) {
+        this.getMediator().getMessage(stateChange,this.name);
+    }
+
+    public void UpCurtains() {
+        System.out.println("I am holding Up Curtains!");
+    }
+}
